7bd3bd23d13e7c9c6020f473bfdc23313bfa9e8a
Vignesh <vigneshch1234@gmail.com>
2026-08-24 13:27:45 +0530

    learning java intial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,38 @@
+public class FirstClass {
+    public static void main(String[] args) {
+//        System.out.println("parking");
+        breakfast();
+        work();
+        lunch();
+        work();
+        dinner();
+        sleep();
+    }
+
+
+    public static void lunch() {
+        System.out.println("rice");
+    }
+
+    public static void work() {
+        System.out.println("laptop");
+    }
+
+    public static void breakfast() {
+        System.out.println("dosa");
+    }
+
+
+    public static void dinner() {
+        System.out.println("sambar");
+    }
+
+    public static void sleep() {
+        System.out.println("gud nyt");
+    }
+
+
+}
+
+
+//structure of basic java program
