53cb46dd6831c0e1180db2aef748e1774bdaeed5
Vignesh <vigneshch1234@gmail.com>
2026-08-25 14:16:40 +0530

    learning print and println

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,2 +1,9 @@
+import java.awt.*;
+
 public class DataTypes {
+    public static void main(String[] args) {
+        System.out.print("HELLO WORLD");
+        System.out.println("HELLO WORLD");
+        System.out.print("HELLO WORLD");
+    }
 }
