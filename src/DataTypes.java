a6204274b5c3289b86eecde723f1944ba6daa3f0
Vignesh <vigneshch1234@gmail.com>
2026-08-25 14:23:43 +0530

    added definiton

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -2,7 +2,9 @@ import java.awt.*;
 
 public class DataTypes {
     public static void main(String[] args) {
+        //print is used to print the string or word in a line, cursor is present in the same line after print the data/string.
         System.out.print("HELLO WORLD");
+        //println is used to print the string or word in a line, then after cursor moved to the next line
         System.out.println("HELLO WORLD");
         System.out.print("HELLO WORLD");
     }
