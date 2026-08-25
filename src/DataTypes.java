f9a4ee877544776ebb3064ed04be763e1bcec1b4
Vignesh <vigneshch1234@gmail.com>
2026-08-25 15:52:31 +0530

    added datatypes

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -7,5 +7,30 @@ public class DataTypes {
         //println is used to print the string or word in a line, then after cursor moved to the next line
         System.out.println("HELLO WORLD");
         System.out.print("HELLO WORLD");
+
+        //variable is used to store the data
+        System.out.println("print to console");
+        //java is case-sensitive
+        //java data types - 8 types primitive -8
+        //byte short
+        int a =35;//mostly used
+        long l = 435L;
+        //double
+        float d = 23.4f;
+        char c = 'v';
+        boolean b = false;
+
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(b);
+        System.out.println(l);
+
     }
 }
