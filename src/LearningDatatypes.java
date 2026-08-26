c35e2ef109b5a05f8e33da21a70d0eefa1e8b105
Vignesh <vigneshch1234@gmail.com>
2026-08-26 10:54:28 +0530

    revision the datatypes

---
+++ src/LearningDatatypes.java
@@ -0,0 +1,34 @@
+public class LearningDatatypes {
+
+
+    public static void main(String[] args) {
+        datatypes();
+        System.out.println("program completed");
+    }
+
+    public static void datatypes() {
+        int a = 35;
+        long l = 435L;
+        float f = 23.4F;
+        char c = 'v';
+        boolean b = false;
+        String value = "print to console";
+
+
+        System.out.println(value);
+        System.out.println(value);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(c);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(c);
+        System.out.println(b);
+    }
+
+
+}
